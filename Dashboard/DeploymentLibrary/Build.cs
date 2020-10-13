725ecdb381bdadfe1bee8d300700a63e4981b83b
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-10-13 19:19:54 +1000

    SM 20201013 First commit

---
+++ Dashboard/DeploymentLibrary/Build.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploymentLibrary
+{
+    public class Build
+    {
+        public string Environment { get; set; }
+        public string BuildNumber {get; set;}
+        public string Product { get; set; }
+    }
+}
